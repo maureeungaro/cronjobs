b20ea9c081519d9df1812b77a20a062743c21cd6
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-04 15:44:22 -0500

    added fairshare script

---
+++ osg_fairshare.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+days='14'
+algo='aging_interleaved'
+half_life_days='0.25'
+history_half_life_days='4'
+queue_penalty='0.5'
+user_bust='5'
+
+ python3 priority_submissions.py \
+ -c ~/msql_conn.txt \
+ -d $days \
+ --priority-algorithm $algo \
+  --half-life-days $half_life_days \
+  --history-half-life-days $history_half_life_days \
+  --queue-penalty-exponent $queue_penalty \
+  --burst-per-user $user_bust \
+  --write-to-db
+
+
