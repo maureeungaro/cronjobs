20b063527e237f4c7f7f5b4851d5e54a13b61e66
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-04 16:05:17 -0500

    added temp osg_fairshare_web.zsh

--- osg_fairshare_web.zsh
+++ osg_fairshare_web.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-
-
+# temp file to be executed on ifarm until web
+# software is installed properly on gemc.jlab.org
 
 days='14'
 algo='aging_interleaved'
@@ -10,16 +10,15 @@ history_half_life_days='4'
 queue_penalty='0.5'
 user_bust='5'
 
+cd /group/clas/www/gemc/html/web_interface/data
 
-
-python3 /home/ungaro/simGrid/db_io/priority_submissions.py \
+python3 /group/clas/www/gemc/html/simGrid/db_io/priority_submissions.py \
  -c ~/msql_conn.txt \
  -d $days \
  --priority-algorithm $algo \
   --half-life-days $half_life_days \
   --history-half-life-days $history_half_life_days \
   --queue-penalty-exponent $queue_penalty \
-  --burst-per-user $user_bust \
-  --write-to-db
+  --burst-per-user $user_bust
 
 
