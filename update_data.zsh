a7c18bd450536b4b43c95fc0f023cdba75e9ac45
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-23 15:40:06 -0400

    added update_data.zsh

---
+++ update_data.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+html_location="$HOME/html"
+repo_names=("clas12-config" "simGrid")
+
+# copy  /group/clas/www/gemc/html/web_interface/data/osgLog.json to the data
+for branch in "${branches[@]}"; do
+	cp /group/clas/www/gemc/html/web_interface/data/osgLog.json $html_location/$branch/web_portal/data
+done
