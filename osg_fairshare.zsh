e0253231ac2dd8b9eddc0aa37448c554dea6aacd
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-25 09:32:31 -0400

    test fairshare mod

--- osg_fairshare.zsh
+++ osg_fairshare.zsh
@@ -16,14 +16,18 @@ queue_penalty='2.0'
 user_bust='2'
 
 cd $path
-/usr/bin/python3 /home/ungaro/simGrid/db_io/priority_submissions.py \
- -c ~/msql_conn.txt \
- -d $days \
- --priority-algorithm $algo \
-  --half-life-days $half_life_days \
-  --history-half-life-days $history_half_life_days \
-  --queue-penalty-exponent $queue_penalty \
-  --burst-per-user $user_bust \
-  --write-to-db
 
+if [[ ! -f $HOME/simGrid/db_io/priority_submissions.py ]]; then
+	echo "simGrid is not installed or not in $HOME. Exiting."
+	exit 1
+fi
 
+/usr/bin/python3 $HOME/simGrid/db_io/priority_submissions.py \
+	-c ~/msql_conn.txt \
+	-d $days \
+	--priority-algorithm $algo \
+	--half-life-days $half_life_days \
+	--history-half-life-days $history_half_life_days \
+	--queue-penalty-exponent $queue_penalty \
+	--burst-per-user $user_bust \
+	--write-to-db
