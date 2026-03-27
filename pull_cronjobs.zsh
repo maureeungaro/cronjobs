1811e98592ee780bc31e28623e146e042e2880e3
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-27 14:00:54 -0400

    updating dir and crontab on the machine and accounts supported

--- pull_cronjobs.zsh
+++ pull_cronjobs.zsh
@@ -1,8 +1,42 @@
 #!/bin/zsh
+set -euo pipefail
 
 cron_location="$HOME/cronjobs"
+user="$(whoami)"
+hmachine=$(hostname -s)
 
 echo
 echo Pulling $cron_location
 cd   $cron_location
 git  pull
+cd
+
+case "$hmachine" in
+	ifarm2401)
+		cronfile="2401.gemc.crontab"
+		;;
+	ifarm2402)
+		cronfile="2402.crontab"
+		;;
+	jlabl2)
+		cronfile="jlabl2.crontab"
+		;;
+	gemc-rh9)
+		case "$user" in
+			ungaro)
+				cronfile="gemc.ungaro.crontab"
+				;;
+			gemc)
+				cronfile="gemc.gemc.crontab"
+				;;
+			*)
+				exit 0
+				;;
+		esac
+		;;
+	*)
+		exit 0
+		;;
+esac
+
+/usr/bin/crontab "$cron_location/$cronfile"
