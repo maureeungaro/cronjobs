fa6a5e8e517875a44cc42de6913a7d66c27adf6c
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-31 15:57:53 -0400

    fixed osgLog

--- osgLog.sh
+++ osgLog.sh
@@ -6,4 +6,6 @@ src3="/volatile/clas12/osg/osg-devel.json"
 
 dest="/u/group/clas/www/gemc/html/web_interface/data/"
 
-cp "$src1" $src2" $src3" "$dest"
+cp "$src1" "$dest"
+cp "$src2" "$dest"
+cp "$src3" "$dest"
