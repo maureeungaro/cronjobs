3ad9b960f6ff427b15ba361b6b8c9be5992500bd
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-31 15:34:20 -0400

    moved osg log to dedicated script

--- osgLog.sh
+++ osgLog.sh
@@ -6,11 +6,4 @@ src3="/volatile/clas12/osg/osg-devel.json"
 
 dest="/u/group/clas/www/gemc/html/web_interface/data/"
 
-logfile="logs/osgLog.txt"
-
 cp "$src1" $src2" $src3" "$dest"
-
-# recreate the log file with current date/time
-rm -f "$logfile"
-date > "$logfile"
-
