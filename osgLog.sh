334414d2c2f0bbf698a043e55329804b8680d065
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-31 14:52:39 -0400

    updated osg logs

--- osgLog.sh
+++ osgLog.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
-src="/volatile/clas12/osg/osgLog.json"
+src1="/volatile/clas12/osg/osgLog.json"
+src2="/volatile/clas12/osg/osg-production.json"
+src3="/volatile/clas12/osg/osg-devel.json"
+
 dest="/u/group/clas/www/gemc/html/web_interface/data/"
+
 logfile="logs/osgLog.txt"
 
-cp "$src" "$dest"
+cp "$src1" $src2" $src3" "$dest"
 
 # recreate the log file with current date/time
 rm -f "$logfile"
