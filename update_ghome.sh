4a092680a76c99e36069b381286aa5f6a88df295
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2026-05-22 16:04:06 -0400

    adding updating gemc documentation dynamically

--- update_ghome.sh
+++ update_ghome.sh
@@ -9,10 +9,8 @@ if [ "$#" -lt 1 ]; then
 fi
 
 milestone="$1"
-
 pymile=/opt/projects/gemc/home/scripts/fetch_github_milestone.py
 out_dir=/opt/projects/gemc/home/_data/github/
-
 
 python3  $pymile \
   --owner gemc \
@@ -20,3 +18,8 @@ python3  $pymile \
   --milestone $milestone \
   --output-dir $out_dir
 
+# also update the documentation dynamically
+ghome=/opt/projects/gemc/home/
+cd $ghome
+export PATH=$PATH:/opt/projects/gemc/g4install/macosx26-clang21-arm64/gemc/dev/bin
+python3 scripts/generate_options_docs.py
