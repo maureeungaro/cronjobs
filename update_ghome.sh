219bdd009666989dadada60ebec953929941d995
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2026-05-24 10:02:31 -0400

    update_ghome.sh

--- update_ghome.sh
+++ update_ghome.sh
@@ -23,3 +23,6 @@ ghome=/opt/projects/gemc/home/
 cd $ghome
 export PATH=$PATH:/opt/projects/gemc/g4install/macosx26-clang21-arm64/gemc/dev/bin
 python3 scripts/generate_options_docs.py
+
+# update bio
+  python3 bio/mauri/scripts/update_material.py --size 1000
